d454898639bb00cc6266fa3b923fc5c71d733497
Esther <estherfinsen@gmail.com>
2024-11-11 13:58:58 +0100

    hej

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import scene from "./imgs/scene.jpeg";
-
 export default function page() {
   return (
     <section className="place-items-center w-9/12 self-center pt-24 md:flex gap-5">
-      <Image src={scene} alt="scene" />
       <h1 className="text-white uppercase text-headers self-end"> FINSENS </h1>
     </section>
   );

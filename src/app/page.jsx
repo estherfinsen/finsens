8958334d66a3c745a32008b0da8a4e2a7db2dcc7
Esther <estherfinsen@gmail.com>
2024-11-20 13:48:13 +0100

    fa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import Image from "next/image";
+import rasmus from "./imgs/rasmus.jpg";
+import Link from "next/link";
+
 export default function Page() {
   return (
     <section className="relative w-full h-screen pt-24">
@@ -5,8 +10,8 @@ export default function Page() {
         <Link href="/grafik" className="relative group aspect-square">
           <div className="relative w-full h-full">
             <Image src={rasmus} alt="grafik" layout="fill" objectFit="cover" className="w-full h-full" />
-            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <p className="text-box_green uppercase text-links">grafik</p>
+            <div className="absolute inset-0 flex justify-center items-center opacity-0 hover:opacity-100 duration-300 bg-box_green hover:w-1/2 hover:left-0 w-0 left-0 transition-all ease-in-out">
+              <p className="text-box_red uppercase text-links">grafik</p>
             </div>
           </div>
         </Link>
@@ -14,8 +19,8 @@ export default function Page() {
         <Link href="/scenografi" className="relative group aspect-square">
           <div className="relative w-full h-full">
             <Image src={rasmus} alt="scenografi" layout="fill" objectFit="cover" className="w-full h-full" />
-            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <p className="text-box_green uppercase text-links">scenografi</p>
+            <div className="absolute inset-0 flex justify-center items-center opacity-0 hover:opacity-100 duration-300 bg-box_green hover:w-1/2 hover:left-0 w-0 left-0 transition-all ease-in-out">
+              <p className="text-box_red uppercase text-links">scenografi</p>
             </div>
           </div>
         </Link>
@@ -23,8 +28,8 @@ export default function Page() {
         <Link href="/speak" className="relative group aspect-square">
           <div className="relative w-full h-full">
             <Image src={rasmus} alt="speak" layout="fill" objectFit="cover" className="w-full h-full" />
-            <div className="absolute inset-0 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              <p className="text-box_green uppercase text-links">speak</p>
+            <div className="absolute inset-0 flex justify-center items-center opacity-0 hover:opacity-100 duration-300 bg-box_green hover:w-1/2 hover:left-0 w-0 left-0 transition-all ease-in-out">
+              <p className="text-box_red uppercase text-links">speak</p>
             </div>
           </div>
         </Link>
